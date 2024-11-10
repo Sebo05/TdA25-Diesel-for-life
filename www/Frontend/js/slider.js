30b20df92f6c4bd4c929cfd0157fea39fe916e16
Sebo <ondryassebo@gmail.com>
2024-11-10 16:52:26 +0100

    Uprava hp
    asd

--- www/Frontend/js/slider.js
+++ www/Frontend/js/slider.js
@@ -13,14 +13,31 @@ function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
+
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
+
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";  
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+
+  slides[slideIndex - 1].style.display = "block";  
+  dots[slideIndex - 1].className += " active";
 }
+
+let autoSlideInterval = setInterval(() => {
+  plusSlides(1);
+}, 5000); // Adjust time (5000 ms = 5 seconds) for faster or slower autoplay
+
+const slideshowContainer = document.querySelector(".slideshow-container");
+slideshowContainer.addEventListener("mouseenter", () => {
+  clearInterval(autoSlideInterval);
+});
+slideshowContainer.addEventListener("mouseleave", () => {
+  autoSlideInterval = setInterval(() => {
+    plusSlides(1);
+  }, 5000);
+});

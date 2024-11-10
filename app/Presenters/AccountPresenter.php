1e024ab5501cd0a76e601c2fb302f745f7b1c4b3
Sebo <ondryassebo@gmail.com>
2024-11-10 23:29:58 +0100

    Idk fakt uz nevim co jsem psat

---
+++ app/Presenters/AccountPresenter.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Presenters;
+use Nette;
+
+class AccountPresenter extends Nette\Application\UI\Presenter
+{
+    public function renderAccount(): void
+    {
+        
+    }
+}
---
+++ app/Presenters/HowPlayPresenter.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Presenters;
+use Nette;
+
+class HowPlayPresenter extends Nette\Application\UI\Presenter
+{
+    public function renderGame(): void
+    {
+        
+    }
+}
